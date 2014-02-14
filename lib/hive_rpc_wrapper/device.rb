b64a1dbf38ab71b5a7dcc619c2088ee242a7f494
Christoph Edthofer <wukerplank@edthofer.at>
2014-02-14 17:01:19 +0100

    socket is in std-lib, no need to add it as dependency; add a few more RPC calls;

--- lib/hive_rpc_wrapper/device.rb
+++ lib/hive_rpc_wrapper/device.rb
@@ -1,5 +1,19 @@
 module HiveRpcWrapper
   class Device < Base
     
+    def configured
+      get({
+        id: generate_request_id,
+        method: "Devices.GetConfiguredDevices"
+      })
+    end
+    
+    def supported
+      get({
+        id: generate_request_id,
+        method: "Devices.GetSupportedDevices"
+      })
+    end
+    
   end
 end
--- lib/hive_rpc_wrapper/plugin.rb
+++ lib/hive_rpc_wrapper/plugin.rb
@@ -1,5 +1,12 @@
 module HiveRpcWrapper
   class Plugin < Base
     
+    def all
+      get({
+        id: generate_request_id,
+        method: "Devices.GetPlugins"
+      })
+    end
+    
   end
 end
