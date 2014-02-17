599815f7a28ec2b1204248b63c5d6122d8f404fa
wukerplank <wukerplank@edthofer.at>
2014-02-17 14:15:29 +0100

    added RPC/API call for ActionType and a test

--- lib/hive_rpc_wrapper/action_type.rb
+++ lib/hive_rpc_wrapper/action_type.rb
@@ -1,5 +1,25 @@
 module HiveRpcWrapper
+  ##
+  # This class wraps everything related to ActionTypes.
+  #
   class ActionType < Base
+    
+    ##
+    # Retrieves all known ActionTypes for a specific Device identified by its +device_id+.
+    #
+    # Example:
+    # 
+    #   HiveRpcWrapper::ActionType.all("{308ae6e6-38b3-4b3a-a513-3199da2764f8}")
+    # 
+    def self.all(device_class_id)
+      get({
+        id: generate_request_id,
+        method: "Devices.GetActionTypes",
+        params: {
+          deviceClassId: device_class_id
+        }
+      })
+    end
     
   end
 end
