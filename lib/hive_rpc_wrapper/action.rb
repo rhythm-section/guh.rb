004dcb88cc9326b8fbcc9276a7a456c06fdd9368
wukerplank <wukerplank@edthofer.at>
2014-02-17 14:46:06 +0100

    added code and tests for Actions

--- lib/hive_rpc_wrapper/action.rb
+++ lib/hive_rpc_wrapper/action.rb
@@ -1,5 +1,28 @@
 module HiveRpcWrapper
+  ##
+  # This class wraps everything related to Actions.
+  # 
   class Action < Base
+    
+    ##
+    # 
+    # Executes a specific Action on a specific Device.
+    # 
+    # To activate an "Elro Power Switch":
+    # 
+    #   HiveRpcWrapper::Action.execute("TODO find specific device id", "{31c9758e-6567-4f89-85bb-29e1a7c55d44}", {power: true})
+    # 
+    def self.execute(device_id, action_type_id, params={})
+      get({
+        id: generate_request_id,
+        method: "Actions.ExecuteAction",
+        params: {
+          deviceId: device_id,
+          actionTypeId: action_type_id,
+          params: params
+        }
+      })
+    end
     
   end
 end
---
+++ spec/integration/action_spec.rb
@@ -0,0 +1,48 @@
+require 'helper'
+
+describe HiveRpcWrapper::Action do
+  
+  before :all do
+    device_class_id = "{308ae6e6-38b3-4b3a-a513-3199da2764f8}"
+    
+    # Create a device
+    response = HiveRpcWrapper::Device.add(device_class_id, {
+      channel1: true,
+      channel2: false,
+      channel3: false,
+      channel4: false,
+      channel5: false,
+      A: true,
+      B: false,
+      C: false,
+      D: false,
+      E: false
+    })
+    response['status'].should eq('success')
+    
+    # Get the newly configured device
+    @device = HiveRpcWrapper::Device.configured['params']['devices'].detect{|d| d['deviceClassId']==device_class_id}
+    @device.should_not be_nil
+    
+    # Get all the possible actions for the device
+    response = HiveRpcWrapper::ActionType.all(device_class_id)
+    response['status'].should eq('success')
+    
+    # Just use the first action
+    @action = response['params']['actionTypes'].first
+  end
+  
+  it "should execute a single action" do
+    response = HiveRpcWrapper::Action.execute(@device['id'], @action['id'], {power: true})
+    
+    response['status'].should eq('success')
+  end
+  
+  it 'should fail if the wrong params are provided' do
+    # response = HiveRpcWrapper::Action.execute(@device['id'], @action['id'], {})
+    # 
+    # response['status'].should eq('error')
+    pending("Don't run this test until we know if the params are optional")
+  end
+  
+end
