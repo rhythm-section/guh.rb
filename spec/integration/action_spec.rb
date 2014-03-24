397a133fe3b36d606303cb160ce5ae31dde23288
wukerplank <wukerplank@edthofer.at>
2014-03-24 15:01:47 +0100

    use the helper to create an elro switch

--- spec/integration/action_spec.rb
+++ spec/integration/action_spec.rb
@@ -3,10 +3,10 @@ require 'helper'
 describe HiveRpcWrapper::Action do
   
   before :all do
+    # Elro Switch
     device_class_id = "{308ae6e6-38b3-4b3a-a513-3199da2764f8}"
     
-    # Create a device
-    response = HiveRpcWrapper::Device.add(device_class_id, {
+    @device = create_configured_device(device_class_id, {
       channel1: true,
       channel2: false,
       channel3: false,
@@ -18,11 +18,6 @@ describe HiveRpcWrapper::Action do
       D: false,
       E: false
     })
-    response['params']['success'].should be_true
-    
-    # Get the newly configured device
-    @device = HiveRpcWrapper::Device.configured['params']['devices'].detect{|d| d['deviceClassId']==device_class_id}
-    @device.should_not be_nil
     
     # Get all the possible actions for the device
     response = HiveRpcWrapper::ActionType.all(device_class_id)
@@ -41,8 +36,8 @@ describe HiveRpcWrapper::Action do
   
   it 'should fail if the wrong params are provided' do
     # response = HiveRpcWrapper::Action.execute(@device['id'], @action['id'], {})
-#     
-#     response['params']['success'].should be_false
+    
+    # response['params']['success'].should be_false
     pending("Don't run this test until we know if the params are optional")
   end
   
